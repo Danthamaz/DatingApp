31952a8d332b5256fb95cf7981564aae8f6ba7d7
Daniel <danielmorrisg@gmail.com>
2024-07-12 10:15:25 -0700

    End of Section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    //primary key in database based on convention (since named Id) 
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+}
